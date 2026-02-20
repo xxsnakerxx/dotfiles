3990ebd40f81d3c062b94615bed6237c50233f32
Dmitrii Kolesnikov <xxsnakerxx@gmail.com>
2026-02-20 15:44:07 +0100

    Add clone_plugin function to zsh.sh for improved plugin management

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -15,9 +15,22 @@ install_oh_my_zsh_plugins() {
 
   ZSH_CUSTOM="${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"
 
-  clone_plugin "https://github.com/zsh-users/zsh-autosuggestions" "zsh-autosuggestions"
-  clone_plugin "https://github.com/zsh-users/zsh-completions" "zsh-completions"
-  clone_plugin "https://github.com/grigorii-zander/zsh-npm-scripts-autocomplete" "zsh-npm-scripts-autocomplete"
+  clone_zsh_plugin "https://github.com/zsh-users/zsh-autosuggestions" "zsh-autosuggestions"
+  clone_zsh_plugin "https://github.com/zsh-users/zsh-completions" "zsh-completions"
+  clone_zsh_plugin "https://github.com/grigorii-zander/zsh-npm-scripts-autocomplete" "zsh-npm-scripts-autocomplete"
 
   success "Oh My Zsh plugins installed"
+}
+
+clone_zsh_plugin() {
+  local url="$1"
+  local name="$2"
+
+  if [[ ! -d "${ZSH_CUSTOM}/${name}" ]]; then
+    info "Cloning ${name} plugin..."
+    git clone "${url}" "${ZSH_CUSTOM}/${name}"
+    success "${name} plugin cloned"
+  else
+    warn "${name} plugin already cloned"
+  fi
 }
